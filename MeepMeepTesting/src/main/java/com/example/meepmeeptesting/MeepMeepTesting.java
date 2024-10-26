c1f56342082631f4e1e56ba6862ea9f7b371ab28
AjayPlus <ajayraghavan@live.com>
2024-10-26 11:27:52 -0700

    Finished auto path for both sides
    finished blue side auto path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,7 +2,9 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Pose2dDual;
+import com.acmerobotics.roadrunner.ProfileParams;
 import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.TrajectoryBuilderParams;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
@@ -42,6 +44,34 @@ public class MeepMeepTesting {
                 .strafeToConstantHeading(new Vector2d(-26.2, -15))
                 .build());
 
+        RoadRunnerBotEntity myBot3 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12.25)
+                .build();
+
+        myBot3.runAction(myBot3.getDrive().actionBuilder(new Pose2d(8, 61.75, Math.toRadians(270)))
+
+                .strafeToConstantHeading(new Vector2d(8, 37.75))
+                .waitSeconds(2.0)
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(47.1, 46.5, Math.toRadians(90)), Math.toRadians(60))
+                .waitSeconds(2.0)
+                .setReversed(false)
+                .strafeToConstantHeading(new Vector2d(58.8, 46.5))
+                .waitSeconds(2.0)
+                .strafeToConstantHeading(new Vector2d(52.1, 46.5))
+                .turnTo(Math.toRadians(140))
+                .waitSeconds(2.0)
+                .turnTo(Math.toRadians(45))
+                .waitSeconds(2.0)
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(28.2, 15, Math.toRadians(180)), Math.toRadians(225))
+                .waitSeconds(1.0)
+                .setReversed(false)
+                .strafeToConstantHeading(new Vector2d(26.2, 15))
+                .build());
+
         RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setColorScheme(new ColorSchemeRedDark())
@@ -51,9 +81,9 @@ public class MeepMeepTesting {
 
         myBot2.runAction(myBot2.getDrive().actionBuilder(new Pose2d(8, -61.75, Math.toRadians(90)))
 
-                .strafeToConstantHeading(new Vector2d(8.5, -37.75))
+                .strafeToConstantHeading(new Vector2d(8, -37.75))
                 .waitSeconds(2.0)
-                .strafeToConstantHeading(new Vector2d(8.5, -39.75))
+                .strafeToConstantHeading(new Vector2d(8, -39.75))
                 .splineToConstantHeading(new Vector2d(34.5, -10.1), Math.toRadians(93))
                 .waitSeconds(0.5)
                 .strafeToConstantHeading(new Vector2d(46, -10.1))
@@ -71,22 +101,34 @@ public class MeepMeepTesting {
 
                 .build());
 
+        RoadRunnerBotEntity myBot4 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12.25)
+                .build();
+
+        myBot4.runAction(myBot4.getDrive().actionBuilder(new Pose2d(-8, 61.75, Math.toRadians(270)))
+
+                .strafeToConstantHeading(new Vector2d(-8, 37.75))
+                .waitSeconds(2.0)
+                .strafeToConstantHeading(new Vector2d(-8, 39.75))
+                .splineToConstantHeading(new Vector2d(-34.5, 10.1), Math.toRadians(273))
+                .waitSeconds(0.5)
+                .strafeToConstantHeading(new Vector2d(-46, 10.1))
+                .waitSeconds(0.5)
+                .strafeToConstantHeading(new Vector2d(-46, 50.1))
+                .waitSeconds(0.5)
+                .strafeToConstantHeading(new Vector2d(-46, 10.1))
+                .strafeToConstantHeading(new Vector2d(-53, 10.1))
+                .strafeToConstantHeading(new Vector2d(-53, 50.1))
+                .waitSeconds(0.5)
+                .strafeToConstantHeading(new Vector2d(-53, 10.1))
+                .strafeToConstantHeading(new Vector2d(-61, 10.1))
+                .strafeToConstantHeading(new Vector2d(-61, 54.5))
 
 
-        new TrajectoryBuilder(
-                new Pose2d(8, -61.75, Math.toRadians(90)),
-                1e-6,
-                0,
-                60, 60,
-                0.5,
-                                                                                                                                                                                                                                                                                                                                Math.toRadians(1),
-                pose -> new Pose2dDual<>(
-                        pose.position.x(),
-                        pose.position.y.unaryMinus(),
-                        pose.position.inverse()
-                )
+                .build());
 
-        ) ;
 
 
 
@@ -96,6 +138,8 @@ public class MeepMeepTesting {
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot1)
                 .addEntity(myBot2)
+                .addEntity(myBot3)
+                .addEntity(myBot4)
                 .start();
     }
 }
